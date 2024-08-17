15da13f2d89035d977779d8f78226fe120bb7eef
zhero <zheronkina86@gmail.com>
2024-08-17 19:05:16 +0300

    Новый проект

---
+++ examinationQuestions/src/main/java/pro/sky/examinationQuestions/ExaminationQuestionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.examinationQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminationQuestionsApplication.class, args);
+	}
+
+}
---
+++ examinationQuestions/src/test/java/pro/sky/examinationQuestions/ExaminationQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.examinationQuestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
